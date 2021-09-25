afcfe63765d41b07ba8b064998491f12649fecd0
Tim Kapitein <tim@newblack.io>
2021-09-25 14:42:34 +0200

    validating args

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,6 +18,13 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
+	countArgs := len(os.Args)
+
+	if countArgs != 2 {
+		printError(errInvalidArguments)
+	}
+
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
