f6ccad5e868be301bce7bb8f0927d786cd84a844
Tim Kapitein <tim@newblack.io>
2021-09-25 14:48:59 +0200

    solution module 1

--- main.go
+++ main.go
@@ -18,14 +18,11 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	countArgs := len(os.Args)
-
-	if countArgs != 2 {
+	if len(os.Args) != 2 {
 		printError(errInvalidArguments)
 	}
 
-	upperOrigin := strings.ToUpper(os.Args[1])
-	originUnit = upperOrigin
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
